bdaf63914f3daf29333004b1ada279e6087e458c
AnasKhan1506 <akanaskhan1506@gmail.com>
2023-07-27 17:42:07 -0700

    updating delete and update Logic

--- adminP.dart
+++ adminP.dart
@@ -1,19 +1,19 @@
 import 'dart:io';
 
-List students = [];
+List<Map<String, dynamic>> students = [];
 
 void main() {
   adminPortal();
 }
 
 void adminlogin() {
-  print("=====Hello Welcome To Admin Portal=====");
+  print("===== Hello Welcome To Admin Portal =====");
   var isLogin = true;
   while (isLogin) {
-    stdout.write("Enter Username : ");
+    stdout.write("Enter Username: ");
     var username = stdin.readLineSync()!;
 
-    stdout.write("Enter Password :");
+    stdout.write("Enter Password: ");
     var password = stdin.readLineSync()!;
 
     if (username == "Admin" && password == "admin123") {
@@ -27,7 +27,7 @@ void adminlogin() {
         print("3. View Students");
         print("4. Delete Student");
         print("5. Exit");
-        stdout.write("Enter your Option : ");
+        stdout.write("Enter your Option: ");
         option = stdin.readLineSync()!;
 
         if (option == "1") {
@@ -35,7 +35,7 @@ void adminlogin() {
         } else if (option == "2") {
           updateStudent();
         } else if (option == "3") {
-          viewStudents();
+          viewStudents(students);
         } else if (option == "4") {
           deleteStudent();
         } else if (option == "5") {
@@ -56,7 +56,7 @@ void adminPortal() {
   adminlogin();
 }
 
-addStudent() {
+void addStudent() {
   print("Enter Student Name:");
   var studentName = stdin.readLineSync();
   print("Enter Student Roll No:");
@@ -74,30 +74,57 @@ addStudent() {
   print("Student added successfully!");
 }
 
-viewStudents() {
-  print("Enter the Student Roll No to view details:");
-  var studentRollNo = stdin.readLineSync();
+void viewStudents(List<Map<String, dynamic>> students) {
+  print("Press 1 if you want to see all the details");
+  print("Press 2 if you want to see specific student detail");
 
-  bool found = false;
-  for (var student in students) {
-    if (student["Student Roll No"] == studentRollNo) {
-      //List k andr access krrhy
+  var press = stdin.readLineSync();
+
+  if (press == "1") {
+    if (students.isEmpty) {
+      print("No students added yet.");
+    } else {
       print("Student Details:");
-      print("Student Name: ${student["Student Name"]}");
-      print(
-          "Student Roll No: ${student["Student Roll No"]}"); // yh 3no value list k andr map ki form m store hui vi
-      print("Student Grade: ${student["Student Grade"]}");
-      found = true;
-      break;
+      for (var student in students) {
+        print("Student Name: ${student["Student Name"]}");
+        print("Student Roll No: ${student["Student Roll No"]}");
+        print("Student Grade: ${student["Student Grade"]}");
+        print("-----------------------");
+      }
     }
-  }
+  } else if (press == "2") {
+    if (students.isEmpty) {
+      print("No students added yet.");
+    } else {
+      print("Enter the Student Roll No to view details:");
+      var studentRollNo = stdin.readLineSync();
 
-  if (!found) {
-    print("Student with Roll No '$studentRollNo' not found.");
+      bool found = false;
+      for (var student in students) {
+        if (student["Student Roll No"] == studentRollNo) {
+          print("Student Details:");
+          print("Student Name: ${student["Student Name"]}");
+          print("Student Roll No: ${student["Student Roll No"]}");
+          print("Student Grade: ${student["Student Grade"]}");
+          found = true;
+          break;
+        }
+      }
+      if (!found) {
+        print("Student with Roll No '$studentRollNo' not found.");
+      }
+    }
+  } else {
+    print("Invalid Option");
   }
 }
 
 void updateStudent() {
+  if (students.isEmpty) {
+    print("No students added yet.");
+    return;
+  }
+
   print("Current List of Students:");
   for (var student in students) {
     print(student["Student Name"]);
@@ -105,55 +132,128 @@ void updateStudent() {
 
   var option = '';
   while (option != "3") {
+    bool cond = true;
     print("What You Want To Update?");
-    print("press 1 for name :");
-    print("press 2 for roll no :");
-    print("press 3 for grade :");
+    print("Press 1 for Name");
+    print("Press 2 for Roll No");
+    print("Press 3 for Grade");
 
-    stdout.write("Enter your Option : ");
-    var option1 = stdin.readLineSync()!;
-    if (option1 == "1") {
+    stdout.write("Enter your Option: ");
+    option = stdin.readLineSync()!;
+
+    if (option == "1") {
       print("Current List of Names:");
       for (var student in students) {
         print(student["Student Name"]);
       }
 
-      //print("What name do you want to update?");
+      stdout.write("Enter the current name: ");
       var oldName = stdin.readLineSync()!;
 
-      var studentToUpdateIndex =
-          students.indexWhere((student) => student["Student Name"] == oldName);
+      var studentToUpdateIndex = students.indexWhere((student) =>
+          student["Student Name"] == oldName); //store oldname in new variable
 
       if (studentToUpdateIndex != -1) {
         stdout.write("Enter the new name: ");
         var newName = stdin.readLineSync()!;
-        students[studentToUpdateIndex]["Student Name"] = newName;
+        students[studentToUpdateIndex]["Student Name"] =
+            newName; //updating old name in new name
+
         print("The name has been updated!");
 
         print("Updated List of Students:");
         for (var student in students) {
           print(student["Student Name"]);
+          cond = false;
         }
+        break; // Add the break statement here to exit the loop after updating the name.
       } else {
         print("Student with the name '$oldName' not found in the list.");
       }
     }
+
+    if (option == "2") {
+      print("Current List of Roll Numbers:");
+      for (var student in students) {
+        print(student["Student Roll No"]);
+      }
+
+      stdout.write("Enter the current Roll No: ");
+      var oldRollNo = stdin.readLineSync()!;
+
+      var studentToUpdateIndex = students
+          .indexWhere((student) => student["Student Roll No"] == oldRollNo);
+
+      if (studentToUpdateIndex != -1) {
+        stdout.write("Enter the new Roll No: ");
+        var newRollNo = stdin.readLineSync()!;
+        students[studentToUpdateIndex]["Student Roll No"] = newRollNo;
+        print("The Roll No has been updated!");
+
+        print("Updated List of Students:");
+        for (var student in students) {
+          print(student["Student Roll No"]);
+          cond = false;
+        }
+        break;
+        // Add the break statement here to exit the loop after updating the Roll No.
+      } else {
+        print("Student with the Roll No '$oldRollNo' not found in the list.");
+      }
+    }
+    // Add similar logic for updating Name and Grade if needed.
+    // Add similar logic for updating Roll No and Grade if needed.
+
+    if (option == "3") {
+      print("Current List of Grades:");
+      for (var student in students) {
+        print(student["Student Grade"]);
+      }
+
+      stdout.write("Enter the current Grade: ");
+      var oldGrade = stdin.readLineSync()!;
+
+      var studentToUpdateIndex = students
+          .indexWhere((student) => student["Student Grade"] == oldGrade);
+
+      if (studentToUpdateIndex != -1) {
+        stdout.write("Enter the new Grade: ");
+        var newGrade = stdin.readLineSync()!;
+        students[studentToUpdateIndex]["Student Grade"] = newGrade;
+        print("The Grade has been updated!");
+
+        print("Updated List of Students:");
+        for (var student in students) {
+          print(student["Student Grade"]);
+          cond = false;
+        }
+        break;
+        // Add the break statement here to exit the loop after updating the Roll No.
+      } else {
+        print("Student with the Roll No '$oldGrade' not found in the list.");
+      }
+    }
   }
+  // Add similar logic for updating Name and Grade if needed.
+  // Add similar logic for updating Roll No and Grade if needed.
 }
 
-deleteStudent() {
-  print("Enter the Student Roll No to Delete his  Records:");
-  var studentNo = stdin.readLineSync();
+void deleteStudent() {
+  if (students.isEmpty) {
+    print("No students added yet.");
+    return;
+  }
+
+  print("Enter the Student Roll No to delete his records:");
+  var studentNo = stdin.readLineSync()!;
   bool found = false;
   for (var student in students) {
     if (student["Student Roll No"] == studentNo) {
-      //List k andr access krrhy
       print("The record is deleting!");
 
-      print(" ${student["Student Name"]}");
-      print(
-          " ${student["Student Roll No"]}"); // yh 3no value list k andr map ki form m store hui vi
-      print(" ${student["Student Grade"]}");
+      print("Student Name: ${student["Student Name"]}");
+      print("Student Roll No: ${student["Student Roll No"]}");
+      print("Student Grade: ${student["Student Grade"]}");
       print("The record has been deleted");
       students
           .removeWhere((element) => student["Student Roll No"] == studentNo);
@@ -166,6 +266,189 @@ deleteStudent() {
     print("Student with Roll No '$studentNo' not found.");
   }
 }
+
+// import 'dart:io';
+
+// List students = [];
+
+// void main() {
+//   adminPortal();
+// }
+
+// void adminlogin() {
+//   print("=====Hello Welcome To Admin Portal=====");
+//   var isLogin = true;
+//   while (isLogin) {
+//     stdout.write("Enter Username : ");
+//     var username = stdin.readLineSync()!;
+
+//     stdout.write("Enter Password :");
+//     var password = stdin.readLineSync()!;
+
+//     if (username == "Admin" && password == "admin123") {
+//       print("Successfully login");
+//       print("======= WELCOME =======");
+//       var option = '';
+//       while (option != "5") {
+//         print("Select from options given below:");
+//         print("1. Add Student");
+//         print("2. Update Student");
+//         print("3. View Students");
+//         print("4. Delete Student");
+//         print("5. Exit");
+//         stdout.write("Enter your Option : ");
+//         option = stdin.readLineSync()!;
+
+//         if (option == "1") {
+//           addStudent();
+//         } else if (option == "2") {
+//           updateStudent();
+//         } else if (option == "3") {
+//           viewStudents(students);
+//         } else if (option == "4") {
+//           deleteStudent();
+//         } else if (option == "5") {
+//           print("Exiting...");
+//           break;
+//         } else {
+//           print("Invalid Option");
+//         }
+//       }
+//       isLogin = false; // Exit the login loop after selecting Exit option
+//     } else {
+//       print("Invalid Credentials");
+//     }
+//   }
+// }
+
+// void adminPortal() {
+//   adminlogin();
+// }
+
+// addStudent() {
+//   print("Enter Student Name:");
+//   var studentName = stdin.readLineSync();
+//   print("Enter Student Roll No:");
+//   var studentRollNo = stdin.readLineSync();
+//   print("Enter Student Grade:");
+//   var studentGrade = stdin.readLineSync();
+
+//   var student = {
+//     "Student Name": studentName,
+//     "Student Roll No": studentRollNo,
+//     "Student Grade": studentGrade
+//   };
+
+//   students.add(student);
+//   print("Student added successfully!");
+// }
+
+// void viewStudents() {
+//   print("Press 1 if you want to see all the details");
+//   print("Press 2 if you want to see specific student detail");
+
+//   var press = stdin.readLineSync();
+
+//   if (press == "1") {
+//     print("Student Details:");
+//     for (var student in students) {
+//       print("Student Name: ${student["Student Name"]}");
+//       print("Student Roll No: ${student["Student Roll No"]}");
+//       print("Student Grade: ${student["Student Grade"]}");
+//       print("-----------------------");
+//     }
+//   } else if (press == "2") {
+//     print("Enter the Student Roll No to view details:");
+//     var studentRollNo = stdin.readLineSync();
+
+//     bool found = false;
+//     for (var student in students) {
+//       if (student["Student Roll No"] == studentRollNo) {
+//         print("Student Details:");
+//         print("Student Name: ${student["Student Name"]}");
+//         print("Student Roll No: ${student["Student Roll No"]}");
+//         print("Student Grade: ${student["Student Grade"]}");
+//         found = true;
+//         break;
+//       }
+//     }
+//     if (!found) {
+//       print("Student with Roll No '$studentRollNo' not found.");
+//     }
+//   } else {
+//     print("Invalid Option");
+//   }
+// }
+
+// void updateStudent() {
+//   print("Current List of Students:");
+//   for (var student in students) {
+//     print(student["Student Name"]);
+//   }
+
+//   var option = '';
+//   while (option != "3") {
+//     print("What You Want To Update?");
+//     print("press 1 for name :");
+//     print("press 2 for roll no :");
+//     print("press 3 for grade :");
+
+//     stdout.write("Enter your Option : ");
+//     var option1 = stdin.readLineSync()!;
+//     if (option1 == "1") {
+//       print("Current List of Names:");
+//       for (var student in students) {
+//         print(student["Student Name"]);
+//       }
+
+//       //print("What name do you want to update?");
+//       var oldName = stdin.readLineSync()!;
+
+//       var studentToUpdateIndex =
+//           students.indexWhere((student) => student["Student Name"] == oldName);
+
+//       if (studentToUpdateIndex != -1) {
+//         stdout.write("Enter the new name: ");
+//         var newName = stdin.readLineSync()!;
+//         students[studentToUpdateIndex]["Student Name"] = newName;
+//         print("The name has been updated!");
+
+//         print("Updated List of Students:");
+//         for (var student in students) {
+//           print(student["Student Name"]);
+//         }
+//       } else {
+//         print("Student with the name '$oldName' not found in the list.");
+//       }
+//     }
+//   }
+// }
+
+// deleteStudent() {
+//   print("Enter the Student Roll No to Delete his  Records:");
+//   var studentNo = stdin.readLineSync();
+//   bool found = false;
+//   for (var student in students) {
+//     if (student["Student Roll No"] == studentNo) {
+//       //List k andr access krrhy
+//       print("The record is deleting!");
+
+//       print(" ${student["Student Name"]}");
+//       print(
+//           " ${student["Student Roll No"]}"); // yh 3no value list k andr map ki form m store hui vi
+//       print(" ${student["Student Grade"]}");
+//       print("The record has been deleted");
+//       students
+//           .removeWhere((element) => student["Student Roll No"] == studentNo);
+//       found = true;
+//       break;
+//     }
+//   }
+
+//   if (!found) {
+//     print("Student with Roll No '$studentNo' not found.");
+//   }
+// }
 // updateStudent() {
 //   var option = "";
 //   print(students);
@@ -196,21 +479,21 @@ deleteStudent() {
 //     }
 //   }
 // }
-  // print("Enter Student Name:");
-  // var studentName = stdin.readLineSync();
-  // print("Enter Student Roll No:");
-  // var studentRollNo = stdin.readLineSync();
-  // print("Enter Student Grade:");
-  // var studentGrade = stdin.readLineSync();
+// print("Enter Student Name:");
+// var studentName = stdin.readLineSync();
+// print("Enter Student Roll No:");
+// var studentRollNo = stdin.readLineSync();
+// print("Enter Student Grade:");
+// var studentGrade = stdin.readLineSync();
 
-  // var student = {
-  //   "Student Name": studentName,
-  //   "Student Roll No": studentRollNo,
-  //   "Student Grade": studentGrade
-  // };
+// var student = {
+//   "Student Name": studentName,
+//   "Student Roll No": studentRollNo,
+//   "Student Grade": studentGrade
+// };
 
-  // students.add(student);
-  // print("Student added successfully!");
+// students.add(student);
+// print("Student added successfully!");
 // viewStudents() {
 //   print("In Order To see Student details");
 //   stdout.write("Enter Your roll no:");
@@ -225,21 +508,20 @@ deleteStudent() {
 //     }
 //   }
 
-  // var viewStudent =
-  //     students.where((e) => e["Student Roll No"] == studentRollNo).toList();
-  // if (viewStudent.isNotEmpty) {
-  //   print("Student Details:");
-  //   for (var students in viewStudent) {
-  //     print("Student Name: ${students["studentName"]}");
-  //   }
-  // }
+// var viewStudent =
+//     students.where((e) => e["Student Roll No"] == studentRollNo).toList();
+// if (viewStudent.isNotEmpty) {
+//   print("Student Details:");
+//   for (var students in viewStudent) {
+//     print("Student Name: ${students["studentName"]}");
+//   }
+// }
 
-  //  else {
-  //   for (var i = 0; i < students.length; i++) {
-  //     print("$i. Student: ${students[i]}");
-  //   }
-  // }
-
+//  else {
+//   for (var i = 0; i < students.length; i++) {
+//     print("$i. Student: ${students[i]}");
+//   }
+// }
 
 /*import 'dart:io';
 
